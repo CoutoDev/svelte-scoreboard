d0733e28a986323b74b0a1094931d663be1a38b8
Vitor Coutinho <vitor.coutinho@rcaturismo.com.br>
2019-12-23 10:43:28 -0300

    First commit

---
+++ src/AddPlayer.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {
+        createEventDispatcher
+    } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    let player = {
+        name: "",
+        points: 0
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        dispatch("addplayer", player);
+        player = {
+            name: "",
+            points: 0
+        };
+    }
+</script>
+
+<form class="grid-3" on:submit={onSubmit}>
+    <input type="text" placeholder="Player Name" bind:value={player.name} />
+    <input type="number" placeholder="Player Points" bind:value={player.points} />
+    <input class="btn btn-primary" type="submit" value="Add Player">
+</form>
